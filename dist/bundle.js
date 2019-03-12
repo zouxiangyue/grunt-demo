c1bb8e0bfb49278ecbcd9330408d579103681835
zouxiangyue <1539442166@qq.com>
2019-03-12 17:20:37 +0800

    concat

--- dist/bundle.js
+++ dist/bundle.js
@@ -17,3 +17,18 @@ $(function() {
   });
 
 });
+;function Rectangle(width, height) {
+    var w = Number(width),
+        h = Number(height);
+
+    this.area = function() {
+          return w * h;
+            
+    };
+
+    this.perimeter = function() {
+          return 2 * (w + h);
+            
+    };
+
+}
